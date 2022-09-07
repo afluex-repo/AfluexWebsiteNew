1bc2fb44c2506f9cc77039ee1bb83db1affd6eb5
Ashwani Mishra <developer6.afluex@gmail.com>
2022-09-07 17:06:16 +0530

    changes

--- Afluex/LandingPageCss/js/footer.js
+++ Afluex/LandingPageCss/js/footer.js
@@ -148,8 +148,6 @@ else {
         return true;
         }
 
-
-
 function Captcha() {
      var alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 	 	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -174,8 +172,6 @@ function Captcha() {
 function removeSpaces(string) {
      return string.split(' ').join('');
 }
-
     </script>
-
 	`);
 
