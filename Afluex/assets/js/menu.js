d81b17541f6144e98f22d78d401dcdb9a274f56e
designer-afluex <68051232+designer-afluex@users.noreply.github.com>
2023-07-05 15:32:47 +0530

    My branch (#109)
    * changes
    
    * chnages
    
    * changes
    
    * changes
    
    * changes
    
    * chnages
    
    * changes
    
    * changes in blog page
    
    * changes
    
    * changes in web layouts
    
    * changes
    
    * changes

---
+++ Afluex/assets/js/menu.js
@@ -0,0 +1,274 @@
+document.write(`
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta charset="utf-8">
+<title>Afluex Multiservices LLP</title>
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+<link href="assets/css/style.css" rel="stylesheet">
+<link href="assets/css/responsive.css" rel="stylesheet">
+<link href="assets/css/css2-14.css?family=Teko:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<link href="assets/css/css2-15.css?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+<link href="assets/css/css2-16.css?family=Pacifico&display=swap" rel="stylesheet">
+<link href="assets/css/css2-17.css?family=Rubik:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+<link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
+<!-- Preloader -->
+<div class="loader-wrap">
+    <div class="preloader">
+        <div class="preloader-close">x</div>
+        <div id="handle-preloader" class="handle-preloader">
+            <div class="animation-preloader">
+                <div class="txt-loading">
+				<div class="">
+						<span data-text-preloader="A" class="letters-loading">
+							A
+						</span>
+						<span data-text-preloader="f" class="letters-loading">
+							F
+						</span>
+						<span data-text-preloader="L" class="letters-loading">
+							L
+						</span>
+						<span data-text-preloader="U" class="letters-loading">
+							U
+						</span>
+						<span data-text-preloader="E" class="letters-loading">
+							E
+						</span>
+						<span data-text-preloader="X" class="letters-loading">
+							X
+						</span>
+						</div>
+						<div>
+						<span data-text-preloader="M" class="letters-loading">
+							M
+						</span>
+						<span data-text-preloader="U" class="letters-loading">
+							U
+						</span>
+						<span data-text-preloader="L" class="letters-loading">
+							L
+						</span>
+						<span data-text-preloader="T" class="letters-loading">
+							T
+						</span>
+						<span data-text-preloader="I" class="letters-loading">
+							I
+						</span>
+						<span data-text-preloader="S" class="letters-loading">
+							S
+						</span>
+						<span data-text-preloader="E" class="letters-loading">
+							E
+						</span>
+						<span data-text-preloader="R" class="letters-loading">
+							R
+						</span>
+						<span data-text-preloader="V" class="letters-loading">
+							V
+						</span>
+						<span data-text-preloader="I" class="letters-loading">
+							I
+						</span>
+						<span data-text-preloader="C" class="letters-loading">
+							C
+						</span>
+						<span data-text-preloader="E" class="letters-loading">
+							E
+						</span>
+						<span data-text-preloader="S" class="letters-loading">
+							S
+						</span>
+						</div>
+						<div class="">
+						<span data-text-preloader="L" class="letters-loading">
+							L
+						</span>
+						<span data-text-preloader="L" class="letters-loading">
+							L
+						</span>
+						<span data-text-preloader="P" class="letters-loading">
+							P
+						</span>
+						</div>
+					</div>
+            </div>  
+        </div>
+    </div>
+</div>
+<!-- Preloader End -->
+<!-- Main Header / Hedare Style Two -->
+    <header class="main-header header-style-two">
+    	
+		<!-- Header Top -->
+        <div class="header-top-two">
+            <div class="auto-container">
+                <div class="inner-container clearfix">
+					<!-- Top Left -->
+					<div class="top-left clearfix">
+					<div class="text"><i class="fa-solid fa-location-dot"></i> D-54, 2nd Floor Arjun Tower Near Ola Office, Vibhuti Khand,
+					Lucknow, Uttar Pradesh 226010</div>
+					</div>
+					
+					<!-- Top Right -->
+                    <div class="top-right pull-right clearfix">
+						<div class="location"><i class="fa fa-envelope-open"></i> supportnow@afluex.com</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+		
+		<!-- Header Lower -->
+        <div class="header-lower">
+        	<div class="auto-container">
+            	<div class="inner-container d-flex justify-content-between align-items-center">
+					
+					<!-- Logo -->
+					<div class="logo pull-left">
+						<div class="logo"><a href="index.html"><img src="assets/images/logo-black.png" class="logo_img" alt="" title=""></a></div>
+					</div>
+					
+					<!-- Nav Outer -->
+					<div class="nav-outer clearfix">
+						<!-- Mobile Navigation Toggler -->
+						<div class="mobile-nav-toggler"><span class="icon flaticon-menu"></span></div>
+						<!-- Main Menu -->
+						<nav class="main-menu navbar-expand-md">
+							<div class="navbar-header">
+								<!-- Toggle Button -->
+								<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+							
+							<div class="navbar-collapse collapse clearfix" id="navbarSupportedContent">
+								<ul class="navigation clearfix">
+                                <li class="current"><a href="#">Home</a>
+                                </li>
+                                <li class="dropdown"><a href="#">About Us</a>
+                                    <ul>
+                                        <li><a href="about.html">Company Overview</a></li>
+                                        <li><a href="team.html">Our Team</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="#">Services</a>
+                                    <ul>
+                                    <li><a href="outdoor-advertising.html">Outdoor Advertising</a></li>
+                                    <li><a href="wall-wrap.html">Wall Wrap Ads</a></li>
+                                    <li><a href="inshop-outdoor-branding.html">Inshop & Outdoor Branding</a></li>
+                                    <li><a href="mobile-van-promotion.html">Mobile Van Promotion</a></li>
+                                    <li><a href="multiplex-advertising.html">Multiplex Advertising</a></li>
+                                    <li><a href="auto-rickshaw-advertising.html">Auto Rickshaw Branding</a></li>
+                                   <li><a href="printing-services.html">Printing Services</a></li>
+                                   <li><a href="tv-advertising.html">TV Advertising</a></li>
+                                   <li><a href="radio-advertising.html">Radio Advertising</a></li>
+                                   <li><a href="fabrication-services.html">Fabrication Services</a></li>
+                                   <li><a href="event-management.html">Event Management</a></li>
+                                   <li><a href="online-marketing.html">Online Marketing</a></li>
+                                   <li><a href="airport-media.html">Airport Media</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="portfolio.html">Portfolio</a></li>
+                                <li><a href="blog.html">Our Blog</a></li>
+                                <li><a href="gallery.html">Our Gallery</a></li>
+                                <li><a href="career.html">Career</a></li>
+                                <li><a href="contact.html">Contact Us</a></li>
+								</ul>
+							</div>
+						</nav>
+					</div>
+				</div>
+            </div>
+        </div>
+        <!-- End Header Lower -->
+        
+		<!-- Sticky Header  -->
+        <div class="sticky-header">
+            <div class="auto-container clearfix">
+                <!--Logo-->
+                <div class="logo pull-left">
+                    <a href="index.html"><img src="assets/images/logo-black.png" class="logo_img" alt="" title=""></a>
+                </div>
+                <!--Right Col-->
+                <div class="pull-right">
+				
+                    <!-- Main Menu -->
+                    <nav class="main-menu">
+                        <!--Keep This Empty / Menu will come through Javascript-->
+                    </nav>
+					<!-- Main Menu End-->
+					
+					<!-- Mobile Navigation Toggler -->
+					<div class="mobile-nav-toggler"><span class="icon flaticon-menu-1"></span></div>
+					
+                </div>
+            </div>
+        </div><!-- End Sticky Menu -->
+    
+		<!-- Mobile Menu  -->
+        <div class="mobile-menu">
+            <div class="menu-backdrop"></div>
+            <div class="close-btn"><span class="icon flaticon-multiply"></span></div>
+            
+            <nav class="menu-box">
+                <div class="nav-logo"><a href="index.html"><img src="assets/images/logo-black.png" class="logo_img" alt="" title=""></a></div>
+                <div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
+            </nav>
+        </div><!-- End Mobile Menu -->
+	
+    </header>
+    <!-- End Main Header -->
+	
+	<!-- Sidebar Cart Item -->
+	<div class="xs-sidebar-group info-group">
+		<div class="xs-overlay xs-bg-black"></div>
+		<div class="xs-sidebar-widget">
+			<div class="sidebar-widget-container">
+				<div class="widget-heading">
+					<a href="#" class="close-side-widget">
+						X
+					</a>
+				</div>
+				<div class="sidebar-textwidget">
+					
+					<!-- Sidebar Info Content -->
+					<div class="sidebar-info-contents">
+						<div class="content-inner">
+							<div class="logo">
+								<a href="index.html"><img src="assets/images/logo-black.png" class="logo_img" alt="" title=""></a>
+							</div>
+							<div class="content-box">
+								<h5>About Us</h5>
+								<p class="text">The argument in favor of using filler text goes something like this: If you use real content in the Process, anytime you reach a review point you’ll end up reviewing and negotiating the content itself and not the design.</p>
+								<a href="contact.html" class="theme-btn btn-style-two"><span class="txt">Consultation</span></a>
+							</div>
+							<div class="contact-info">
+								<h5>Contact Info</h5>
+								<ul class="list-style-one">
+									<li><span class="icon fas fa-map-marker"></span>Chicago 12, Melborne City, USA</li>
+									<li><span class="icon fas fa-phone"></span>(111) 111-111-1111</li>
+									<li><span class="icon fas fa-envelope"></span>aginco@gmail.com</li>
+									<li><span class="icon fas fa-clock"></span>Week Days: 09.00 to 18.00 Sunday: Closed</li>
+								</ul>
+							</div>
+							<!-- Social Box -->
+							<ul class="social-box">
+								<li><a href="../index-16.htm" class="fa fa-facebook-f"></a></li>
+								<li><a href="../index-17.htm" class="fa fa-twitter"></a></li>
+								<li><a href="../index-21.htm" class="fa fa-dribbble"></a></li>
+								<li><a href="../index-22.htm" class="fa fa-linkedin"></a></li>
+							</ul>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END sidebar widget item -->
+
+
+`);
