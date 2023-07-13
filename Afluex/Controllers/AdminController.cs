577559fc5f5cdba364ed77e1fb74418d8b44ebb7
afluex-sudhakar <79094025+afluex-sudhakar@users.noreply.github.com>
2023-07-13 10:50:40 +0530

    changes (#110)

--- Afluex/Controllers/AdminController.cs
+++ Afluex/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
-﻿using Afluex.Models;
+﻿using Afluex.Filter;
+using Afluex.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,10 +15,10 @@ namespace Afluex.Controllers
         // GET: Admin
         public ActionResult DashBoard()
         {
-        
+
             return View();
         }
-      
+
         public ActionResult PostBlog(string ID)
         {
             if (ID != null)
@@ -41,7 +43,7 @@ namespace Afluex.Controllers
                     Obj.Title = r["Title"].ToString();
                     Obj.ShortDecription = r["ShortDecription"].ToString();
                     Obj.Decription = r["Description"].ToString();
-                    Obj.Pk_BlogId= r["Pk_BlogId"].ToString();
+                    Obj.Pk_BlogId = r["Pk_BlogId"].ToString();
                     Obj.AddedBy = r["Name"].ToString();
                     Obj.FileUpload = r["AddedOn"].ToString();
 
@@ -52,7 +54,7 @@ namespace Afluex.Controllers
             }
             return View(obj);
         }
-     
+
         public ActionResult DeleteBlog(string id)
         {
             try
@@ -127,5 +129,264 @@ namespace Afluex.Controllers
             }
             return View(obj);
         }
+
+        public ActionResult CareerOpportunities(string Id)
+        {
+            Blog model = new Blog();
+            if (Id != null)
+            {
+                model.PK_CareerOpporID = Id;
+                DataSet ds = model.GetCareerOpportunitiesList();
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    model.PK_CareerOpporID = ds.Tables[0].Rows[0]["PK_CareerOpporID"].ToString();
+                    model.MVCDeveloper = ds.Tables[0].Rows[0]["MVCDeveloper"].ToString();
+                    model.MobileApplication = ds.Tables[0].Rows[0]["MobileApplication"].ToString();
+                    model.Graphics = ds.Tables[0].Rows[0]["Graphics"].ToString();
+                    model.SearchEngine = ds.Tables[0].Rows[0]["SearchEngine"].ToString();
+                    model.Marketing = ds.Tables[0].Rows[0]["Marketing"].ToString();
+                    model.Description = ds.Tables[0].Rows[0]["Description"].ToString();
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("CareerOpportunities")]
+        [OnAction(ButtonName = "btnsave")]
+        public ActionResult CareerOpportunities(Blog model)
+        {
+            try
+            {
+                model.AddedBy = Session["Pk_AdminId"].ToString();
+                DataSet ds = model.CareerOpportunities();
+                if (ds != null && ds.Tables[0].Rows.Count > 0)
+                {
+                    if (ds.Tables[0].Rows[0]["Msg"].ToString() == "1")
+                    {
+                        TempData["Careermsg"] = "Career Opportunities Saved Successfully !!";
+                    }
+                    else
+                    {
+                        TempData["Careermsg"] = ds.Tables[0].Rows[0]["ErrorMessage"].ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return RedirectToAction("CareerOpportunities", "Admin");
+        }
+
+
+        [HttpPost]
+        [ActionName("CareerOpportunities")]
+        [OnAction(ButtonName = "btnupdate")]
+        public ActionResult UpdateCareerOpportunities(Blog model)
+        {
+            try
+            {
+                model.AddedBy = Session["Pk_AdminId"].ToString();
+                DataSet ds = model.UpdateCareerOpportunities();
+                if (ds != null && ds.Tables[0].Rows.Count > 0)
+                {
+                    if (ds.Tables[0].Rows[0]["Msg"].ToString() == "1")
+                    {
+                        TempData["Careermsg"] = "Career Opportunities Updated Successfully !!";
+                    }
+                    else
+                    {
+                        TempData["Careermsg"] = ds.Tables[0].Rows[0]["ErrorMessage"].ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return RedirectToAction("CareerOpportunities", "Admin");
+        }
+
+
+        public ActionResult CareerOpportunitiesList(Blog obj)
+        {
+            List<Blog> lst = new List<Blog>();
+            DataSet ds = obj.GetCareerOpportunitiesList();
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow r in ds.Tables[0].Rows)
+                {
+                    Blog Obj = new Blog();
+                    Obj.PK_CareerOpporID = r["PK_CareerOpporID"].ToString();
+                    Obj.MVCDeveloper = r["MVCDeveloper"].ToString();
+                    Obj.MobileApplication = r["MobileApplication"].ToString();
+                    Obj.Graphics = r["Graphics"].ToString();
+                    Obj.SearchEngine = r["SearchEngine"].ToString();
+                    Obj.Marketing = r["Marketing"].ToString();
+                    Obj.Description = r["Description"].ToString();
+                    lst.Add(Obj);
+                }
+                obj.lstcareer = lst;
+            }
+            return View(obj);
+        }
+
+        public ActionResult OpenVacancy(Blog model, string Id)
+        {
+            if (Id != null)
+            {
+                model.PK_VacancyID = Id;
+                DataSet ds = model.GetOpenVacancyList();
+                if (ds != null && ds.Tables[0].Rows.Count > 0)
+                {
+                    model.Designation = ds.Tables[0].Rows[0]["Designation"].ToString();
+                    model.MinExperience = ds.Tables[0].Rows[0]["MinExperience"].ToString();
+                    model.SalaryRange = ds.Tables[0].Rows[0]["SalaryRange"].ToString();
+                    model.UploadFile = ds.Tables[0].Rows[0]["UploadFile"].ToString();
+                    model.TotalVacancy = ds.Tables[0].Rows[0]["TotalVacancy"].ToString();
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("OpenVacancy")]
+        [OnAction(ButtonName = "btnsave")]
+        public ActionResult OpenVacancy(Blog model, HttpPostedFileBase postedFile)
+        {
+            try
+            {
+                if (postedFile != null)
+                {
+                    model.UploadFile = "/UploadFile/" + Guid.NewGuid() + Path.GetExtension(postedFile.FileName);
+                    postedFile.SaveAs(Path.Combine(Server.MapPath(model.UploadFile)));
+                }
+                model.AddedBy = Session["Pk_AdminId"].ToString();
+                DataSet ds = model.SaveOpenVacancy();
+                if (ds != null && ds.Tables[0].Rows.Count > 0)
+                {
+                    if (ds.Tables[0].Rows[0]["Msg"].ToString() == "1")
+                    {
+                        TempData["OpenVacancy"] = "Open Vacancy Saved Successfully !!";
+                    }
+                    else
+                    {
+                        TempData["OpenVacancy"] = ds.Tables[0].Rows[0]["ErrorMessage"].ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return RedirectToAction("OpenVacancy", "Admin");
+        }
+
+
+        public ActionResult OpenVacancyList(Blog obj)
+        {
+            List<Blog> lst = new List<Blog>();
+            DataSet ds = obj.GetOpenVacancyList();
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow r in ds.Tables[0].Rows)
+                {
+                    Blog Obj = new Blog();
+                    Obj.Designation = r["Designation"].ToString();
+                    Obj.PK_VacancyID = r["PK_VacancyID"].ToString();
+                    Obj.MinExperience = r["MinExperience"].ToString();
+                    Obj.SalaryRange = r["SalaryRange"].ToString();
+                    Obj.UploadFile = r["UploadFile"].ToString();
+                    Obj.TotalVacancy = r["TotalVacancy"].ToString();
+                    lst.Add(Obj);
+                }
+                obj.lstvacancy = lst;
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ActionName("OpenVacancy")]
+        [OnAction(ButtonName = "btnupdate")]
+        public ActionResult UpdateOpenVacancy(Blog model, HttpPostedFileBase postedFile)
+        {
+            try
+            {
+
+                if (postedFile != null)
+                {
+                    model.UploadFile = "/UploadFile/" + Guid.NewGuid() + Path.GetExtension(postedFile.FileName);
+                    postedFile.SaveAs(Path.Combine(Server.MapPath(model.UploadFile)));
+                    
+                }
+                model.AddedBy = Session["Pk_AdminId"].ToString();
+                DataSet ds = model.UpdateOpenVacancy();
+                if (ds != null && ds.Tables[0].Rows.Count > 0)
+                {
+                    if (ds.Tables[0].Rows[0]["Msg"].ToString() == "1")
+                    {
+                        TempData["OpenVacancy"] = "Open Vacancy Updated Successfully !!";
+                    }
+                    else
+                    {
+                        TempData["OpenVacancy"] = ds.Tables[0].Rows[0]["ErrorMessage"].ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return RedirectToAction("OpenVacancy", "Admin");
+        }
+
+
+
+        public ActionResult DeleteOpenVacancy(Blog model, string Id)
+        {
+            try
+            {
+                if (Id != null)
+                {
+                    model.PK_VacancyID = Id;
+                    model.AddedBy = Session["Pk_AdminId"].ToString();
+                    DataSet ds = model.DeleteOpenVacancy();
+                    if (ds != null && ds.Tables[0].Rows.Count > 0)
+                    {
+                        if (ds.Tables[0].Rows[0]["Msg"].ToString() == "1")
+                        {
+                            model.Result = "yes";
+                            TempData["OpenVacancy"] = "Open Vacancy details Deleted Successfully !!";
+                        }
+                        else
+                        {
+                            TempData["OpenVacancy"] = ds.Tables[0].Rows[0]["ErrorMessage"].ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Json(model,JsonRequestBehavior.AllowGet);
+           
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
